42ebab117e802bec07cda6594c359c1918bbb8ed
sarpong <abasimisarpong@rocketmail.com>
2019-08-19 12:55:00 +0100

    animated colour

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -4,9 +4,13 @@ import { useSpring, animated } from 'react-spring';
 
 const MyFunctionComponent = ()=>{
   const divAnimation = useSpring({ opacity:1 ,from:{opacity: 0}})
+  const BAnimation = useSpring({ background:'green' ,from:{background: 'gold'}})
+  const numbers = useSpring({ number:2 , from:{number: 10}})
   return(
     <animated.div style={divAnimation}>
       <h1>Will I be animated?</h1>
+      <animated.h1>{numbers.number}</animated.h1>
+      <animated.span style={ BAnimation}>hahah</animated.span>
     </animated.div>
   );
 }
